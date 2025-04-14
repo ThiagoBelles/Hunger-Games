cd1c1f01de395a08c531db3742e1ef265892a9b7
Thiago Belles <belles.dev@gmail.com>
2025-04-14 15:26:14 -0300

    First Commit

---
+++ fotos.js
@@ -0,0 +1,13 @@
+const modal = document.getElementById("modal")     // <- Seleciona o Modal (fundo escuro)
+let modalContent = document.getElementById('modal-content');  // <- Seleciona o Modal Content (imagens)
+
+
+function openModal(img){                              // Função de abrir o Modal 
+    modal.style.visibility = 'visible'
+    modalContent.src = img
+}
+
+function hideModal(){                              // Função de esconder o Modal
+    modal.style.visibility = 'hidden'
+}
+
